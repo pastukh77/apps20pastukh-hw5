2e4062132f233e0f5e6e864eaaf0f1efc421c20a
yluchka <pastukh@ucu.edu.ua>
2020-12-12 21:40:23 +0200

    Streams  initial commit

---
+++ src/main/java/ua/edu/ucu/stream/AsIntStream.java
@@ -0,0 +1,122 @@
+package ua.edu.ucu.stream;
+
+import ua.edu.ucu.function.*;
+
+import java.util.ArrayList;
+
+import static java.lang.Integer.compare;
+
+public class AsIntStream implements IntStream {
+    private final int[] integerArray;
+
+    private AsIntStream(int... values) {
+        integerArray = values;
+    }
+
+    public static IntStream of(int... values) {
+        return new AsIntStream(values);
+    }
+
+    @Override
+    public Integer sum() {
+        int sum = 0;
+        for (int val : integerArray) {
+            sum += val;
+        }
+        return sum;
+    }
+
+    @Override
+    public long count() {
+        return integerArray.length;
+    }
+
+    @Override
+    public Double average() {
+        return (double) (sum() / count());
+    }
+
+    private Integer maxOrMin(int sign) {
+        int current = integerArray[0];
+        for (int i = 0; i < count() - 1; i++) {
+            int next = integerArray[i + 1];
+            if (sign * compare(current, next) < 0) {
+                current = next;
+            }
+        }
+        return current;
+    }
+
+    @Override
+    public Integer max() {
+        return maxOrMin(1);
+    }
+
+    @Override
+    public Integer min() {
+        return maxOrMin(-1);
+    }
+
+    @Override
+    public IntStream filter(IntPredicate predicate) {
+        int[] result = new int[(int) count()];
+        int newSize = 0;
+        int val;
+        for (int i = 0; i < count(); i++) {
+            val = integerArray[i];
+            if (predicate.test(val)) {
+                result[newSize] = val;
+                newSize += 1;
+            }
+        }
+        int[] finalResult = new int[newSize];
+        if (newSize >= 0) System.arraycopy(result, 0, finalResult, 0, newSize);
+        return new AsIntStream(finalResult);
+    }
+
+    @Override
+    public void forEach(IntConsumer action) {
+        for (int val : integerArray) {
+            action.accept(val);
+        }
+    }
+
+    @Override
+    public IntStream map(IntUnaryOperator mapper) {
+        for (int i = 0; i < count(); i++) {
+            integerArray[i] = mapper.apply(integerArray[i]);
+        }
+        return new AsIntStream(integerArray);
+    }
+
+    @Override
+    public IntStream flatMap(IntToIntStreamFunction func) {
+        int flatSize = (int) (func.applyAsIntStream(
+                integerArray[0]).count() * count());
+        int[] result = new int[flatSize];
+        int counter = 0;
+        for (int i = 0; i < count(); i++) {
+            IntStream flattened = func.applyAsIntStream(integerArray[i]);
+            int[] flattenedArray = flattened.toArray();
+            for (int val : flattenedArray) {
+                result[counter++] = val;
+            }
+
+        }
+        return new AsIntStream(result);
+    }
+
+    @Override
+    public int reduce(int identity, IntBinaryOperator op) {
+        int result = identity;
+        for (int val : integerArray) {
+            result = op.apply(result, val);
+        }
+        return result;
+    }
+
+    @Override
+    public int[] toArray() {
+        return integerArray;
+    }
+}
---
+++ src/test/java/ua/edu/ucu/AsIntTest.java
@@ -0,0 +1,32 @@
+package ua.edu.ucu;
+
+import ua.edu.ucu.stream.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class AsIntTest {
+    private IntStream intStream;
+
+    @Before
+    public void init() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+    }
+
+    @Test
+    public void testAsIntStreamSum() {
+        int expResult = 5;
+        int result = intStream.sum();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAsIntStreamAverage() {
+        double expResult = 1.0;
+        double result = intStream.average();
+        assertEquals(expResult, result, 0.00001);
+    }
+}
